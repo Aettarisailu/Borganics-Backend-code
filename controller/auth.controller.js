bff12e9db0ffabfe6f609a1ef2f42fb251b1d906
ananisai <ranransai992@gmail.com>
2024-07-09 17:32:49 +0530

    Replace bcrypt with bcryptjs

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,5 @@
 const UserModel = require('../model/user.model');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 
